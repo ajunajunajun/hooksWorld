57e9b49059e6b3b3aa67ed308330152d77354191
ajunajunajun <ajunnajunn@gmail.com>
2019-07-31 15:06:06 +0900

    use useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useContext, useCallback } from 'react'
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+  useMemo
+} from 'react'
 
 type Props = {
   initial: number
@@ -8,6 +14,7 @@ export const testContext = React.createContext({ context: 'contextdayo~' })
 
 export default ({ initial = 0 }: Props) => {
   const [count, setCount] = useState(initial)
+  const [num, setNum] = useState(1)
   const { context } = useContext(testContext)
 
   useEffect(() => {
@@ -16,14 +23,21 @@ export default ({ initial = 0 }: Props) => {
     return () => console.log('willunmount(副作用時にも発火)')
   }, [context])
 
-  const increment = useCallback(() => setCount(count => count + 1), [])
-  const decrement = useCallback(() => setCount(count => count - 1), [])
+  // めっちゃ高価な計算
+  const dec_num_1: number = useMemo(() => num - 1, [num])
+
+  const increment = useCallback(() => setCount(count => count + num), [num])
+  const decrement = useCallback(() => setCount(count => count - num), [num])
+  const increment_num = useCallback(() => setNum(num => num + 1), [])
+  const decrement_num = useCallback(() => setNum(dec_num_1), [dec_num_1])
 
   return (
     <>
       {count}
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>+1</button>
+      <button onClick={increment}>+{num}</button>
+      <button onClick={decrement}>-{num}</button>
+      <button onClick={increment_num}>{num + 1}づつ変わるようになるよ</button>
+      <button onClick={decrement_num}>{num - 1}づつ変わるようになるよ</button>
     </>
   )
 }
