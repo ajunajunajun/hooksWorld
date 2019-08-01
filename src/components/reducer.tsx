a8ca45a502b9f0fe6a88c5ff2fca482d2e55bc9f
ajunajunajun <ajunnajunn@gmail.com>
2019-08-01 10:49:22 +0900

    use useReducer

---
+++ src/components/reducer.tsx
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react'
+
+const initialState = { count: 0 as number }
+
+type ActionType = {
+  type: 'reset' | 'decrement' | 'increment'
+}
+
+const reducer = (state: any, action: ActionType) => {
+  switch (action.type) {
+    case 'reset':
+      return initialState
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
+
+export const ReducerComponent = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  )
+}
+
+export default ReducerComponent
